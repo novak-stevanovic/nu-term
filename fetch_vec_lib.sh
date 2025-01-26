181538ce977ef0c70c241509d8a21e121680c050
novak-stevanovic <novakst24@gmail.com>
2025-01-26 15:49:15 +0100

    change in script

--- fetch_vec_lib.sh
+++ fetch_vec_lib.sh
@@ -4,6 +4,6 @@ git clone https://github.com/novak-stevanovic/generic-data-structures
 cp generic-data-structures/include/gds_vector.h nu-term/include/lib
 cp generic-data-structures/include/gds_array.h nu-term/include/lib
 cd generic-data-structures
-make
+make clean && make
 cd ..
 cp generic-data-structures/libgds.so nu-term/lib
